04802c60ce3224f0d3dac29441f67d7a38e8e710
Shubham Vishwakarma <shubhamindokh@gmail.com>
2025-11-16 02:03:22 +0530

    connect to  mongo atlas

---
+++ src/index.js
@@ -0,0 +1,50 @@
+// require('dotenv').config({path: 
+//    './env'
+// });
+import dotenv from "dotenv"
+import connectDB from './db/index.js';
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+; (async () => {
+    try {
+        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERR: ", error);
+            throw error
+
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`Appmis lostening on port ${process.env.PORT}`);
+
+        })
+    } catch (error) {
+        console.error("ERROR: ", error);
+        throw err
+    }
+})()
+    */
